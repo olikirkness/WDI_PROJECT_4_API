c937e6d4499f6cbe6eb33e1b6c42b7c2f8822cbc
olikirkness <oli@kirkness.com>
2017-09-01 12:29:28 +0100

    added back end authentication

---
+++ db/migrate/20170901110118_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :first_name
+      t.string :last_name
+      t.text :image
+      t.integer :ranking
+      t.string :password_digest
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
