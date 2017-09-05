9218062c06e195bafdd73fc846811f5f87408149
olikirkness <oli@kirkness.com>
2017-09-05 17:23:30 +0100

    small changes

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,5 +1,5 @@
 class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :sender_id, :reciever_id, :league
+  attributes :id, :sender_id, :reciever_id, :league, :sender, :receiver
   belongs_to :league
   belongs_to :sender, foreign_key: :sender_id, class_name: "User"
   belongs_to :receiver, foreign_key: :reciever_id, class_name: "User"
