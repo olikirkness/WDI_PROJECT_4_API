e9404f6dfcc86c667109da049051c48dcc98ee33
olikirkness <oli@kirkness.com>
2017-10-09 18:28:15 +0100

    added created at for notification data

--- app/serializers/challenge_serializer.rb
+++ app/serializers/challenge_serializer.rb
@@ -1,5 +1,5 @@
 class ChallengeSerializer < ActiveModel::Serializer
-  attributes :id, :sender_id, :reciever_id, :league, :sender, :receiver
+  attributes :id, :sender_id, :reciever_id, :league, :sender, :receiver, :created_at
   belongs_to :league
   belongs_to :sender, foreign_key: :sender_id, class_name: "User"
   belongs_to :receiver, foreign_key: :reciever_id, class_name: "User"
